0b57bdc7cd250f1e0be0f5804a32290ae7e5c64f
hashimtech <hashimtech336@gmail.com>
2025-07-31 09:59:50 +0530

    login page UI completed with textformfields for username and password,clickable login in container and 'Forgot Password' & 'Create a account' as clickable these all are in a container with padding under scaffold

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,6 +10,75 @@ class HomePage extends StatelessWidget {
         color: Colors.white,
         height: MediaQuery.sizeOf(context).height,
         width: MediaQuery.sizeOf(context).width,
+        padding: EdgeInsets.all(100),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hint: Text('username'),
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hint: Text('password'),
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility_off),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment(0, 0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+
+                width: 100,
+                height: 30,
+
+                child: Text('Login'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Create a account',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
